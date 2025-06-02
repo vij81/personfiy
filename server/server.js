0832f66507de67a0a3ea691cc9187936631694c1
Vijeta Verma <vijetaverma81@gmail.com>
2025-06-02 12:45:27 +0530

    Prepare for Render deployment

--- server/server.js
+++ server/server.js
@@ -49,6 +49,12 @@ app.use("/bills", billRoutes);
 const notify = require("./routes/notifications");
 app.use("/notifications", notify);
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
 
 app.listen(8080, () => console.log('Server running on port 8080'));
 
