dd610c2294d468ec3372285f992938d20b22ab85
Vijeta Verma <vijetaverma81@gmail.com>
2025-06-03 23:27:57 +0530

    Descriptive commit message

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+// const API = axios.create({
+//   baseURL: 'http://localhost:5000',
+// });
+// Use environment variable in React
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://personfiy.onrender.com',
+  withCredentials: true,
 });
+
 
 export const login = (formData) => API.post('/auth/login', formData);
 export const register = (formData) => API.post('/auth/register', formData);
