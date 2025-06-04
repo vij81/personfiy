caebc5bd74c1a4ca5ad77c607d576a0a02d5c267
Vijeta Verma <vijetaverma81@gmail.com>
2025-06-04 17:20:22 +0530

    netifly

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const authRoutes = require('./routes/auth');
 const TransactionRoutes = require('./routes/transactions');
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://personfiy.netlify.app/', credentials: true }));
 app.use(express.json());
 
 app.use(
